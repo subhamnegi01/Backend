e3ea72f10e7d9c8e9c005cb92ac7f6dfd892bca7
Subham Negi <negisubham210@gmail.com>
2026-09-04 12:19:25 +0530

    update

--- 01/first.js
+++ 01/first.js
@@ -1,1 +1,10 @@
+const {sum, sub} = require("./second.js")
+
+
+
 console.log("Hello, World!");
+
+sum(5, 10);
+sub(10, 5);
+// CJS: Common JS module system
+// I need second.js code in my first.js file
--- 01/second.js
+++ 01/second.js
@@ -1,1 +1,13 @@
+
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+function sub(a,b){
+    console.log(a-b);
+}
+
+module.exports = { sum, sub };
+
 console.log("Hello India!");
