4c364344978d09bf0d848d294a050ea754130257
Subham Negi <negisubham210@gmail.com>
2026-09-05 22:43:16 +0530

    update

--- 02/sour/Today/calculator/index.js
+++ 02/sour/Today/calculator/index.js
@@ -1,6 +1,6 @@
-const sum = require("./sum")
-const sub = require("./sub")
-const mul = require("./mul")
+const sum = require("./sum");
+const sub = require("./sub");
+const mul = require("./mul");
 
 
 sum(2,4)
