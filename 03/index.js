efcc6c7c1dc23068e4056dead9a1cca52171541e
Subham Negi <negisubham210@gmail.com>
2026-09-06 11:54:33 +0530

    commit

---
+++ 03/index.js
@@ -0,0 +1,15 @@
+let a= 10;
+let b = "Hello world";
+
+console.log(b)
+
+function sum(a,b){
+    return (a+b);
+}
+
+setTimeout(() => {
+    console.log("Hello India")
+}, 3000);
+
+console.log(a)
+console.log(sum(10,20))
